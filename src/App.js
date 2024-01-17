6fe114a7dbd6a9c70a17f4efdad83045ece298f4
Prashanth14 <kadireprashanthreddy1@gmail.com>
2024-01-17 10:57:56 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='text-3xl font-bold text-green-800'>
+      Hello Everyone! This is Prashanth Reddy Kadire!!
+    </div>
+  );
+}
+
+export default App;
